2fceb1d13721b31e8bfe99a590269276ab129eb0
bartwisniewski <bartekwisnia@op.pl>
2022-11-15 09:14:19 +0100

    user management - backup - in progress

--- source/core/vpcoreproject/users/models.py
+++ source/core/vpcoreproject/users/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class FamilySize(models.Model):
+    adults = models.IntegerField(default=1)
+    children = models.IntegerField(default=0)
+    infants = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"family contains, adults: {self.adults}, children: {self.children}, infants: {self.infants}"
+
+
+class MyUser(AbstractUser):
+    default_family = models.OneToOneField(FamilySize, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class SocialMedia(models.Model):
+    name = models.CharField(max_length=30)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class SocialUser(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    social_media = models.ForeignKey(SocialMedia, on_delete=models.CASCADE)
+    social_name = models.CharField(max_length=50)
+    social_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.user.username} on {self.social_media.name} is {self.social_name}"
