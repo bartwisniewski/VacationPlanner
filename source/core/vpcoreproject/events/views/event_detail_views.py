8a290ee15368af1a2dafdc5dbd796b8152f23269
bartwisniewski <bartekwisnia@op.pl>
2022-12-21 17:26:26 +0100

    test commit

--- source/core/vpcoreproject/events/views/event_detail_views.py
+++ source/core/vpcoreproject/events/views/event_detail_views.py
@@ -5,17 +5,24 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.db.models import Count, Case, Value, BooleanField, When
 
-from events.models import Event, UserToEvent, DateProposal, DateProposalVote, PlaceProposal, PlaceProposalVote
+from events.models import (
+    Event,
+    UserToEvent,
+    DateProposal,
+    DateProposalVote,
+    PlaceProposal,
+    PlaceProposalVote,
+)
 
 
 class EventDetailView(UserPassesTestMixin, DetailView):
     model = Event
     template_suffix_from_status = ["_detail_0", "_detail_1", "_detail_2"]
-    success_url = reverse_lazy('events-list')
-    permission_denied_message = f'you are not participant of this event'
+    success_url = reverse_lazy("events-list")
+    permission_denied_message = f"you are not participant of this event"
 
     def get_event(self, request):
-        event_id = self.kwargs['pk']
+        event_id = self.kwargs["pk"]
         return Event.get_or_warning(event_id, request)
 
     def test_func(self):
@@ -28,7 +35,9 @@ class EventDetailView(UserPassesTestMixin, DetailView):
 
     def set_template_name_suffix(self):
         if 0 <= self.object.status <= len(EventDetailView.template_suffix_from_status):
-            self.template_name_suffix = self.template_suffix_from_status[self.object.status]
+            self.template_name_suffix = self.template_suffix_from_status[
+                self.object.status
+            ]
 
     def get_votes(self, context, proposals, vote_model):
         event_votes = vote_model.objects.filter(proposal__in=proposals)
@@ -43,29 +52,44 @@ class EventDetailView(UserPassesTestMixin, DetailView):
             i_voted=Case(
                 When(id__in=my_voted, then=Value(True)),
                 default=Value(False),
-                output_field=BooleanField()
+                output_field=BooleanField(),
             )
         )
 
     def get_proposals(self, context, lookup_vote_model, proposal_model):
-        proposals = proposal_model.objects.filter(user_event__event=self.object).annotate(Count(lookup_vote_model)) \
-            .order_by(f'-{lookup_vote_model}__count')
-        proposals = self.annotate_i_voted(proposals=proposals, lookup_vote_model=lookup_vote_model)
+        proposals = (
+            proposal_model.objects.filter(user_event__event=self.object)
+            .annotate(Count(lookup_vote_model))
+            .order_by(f"-{lookup_vote_model}__count")
+        )
+        proposals = self.annotate_i_voted(
+            proposals=proposals, lookup_vote_model=lookup_vote_model
+        )
         return proposals
 
     def get_context_0(self, context):
-        proposals = self.get_proposals(context=context, lookup_vote_model='dateproposalvote',
-                                       proposal_model=DateProposal)
-        my_votes = self.get_votes(context=context, vote_model=DateProposalVote, proposals=proposals)
-        context['my_votes'] = my_votes
-        context['proposals'] = proposals
+        proposals = self.get_proposals(
+            context=context,
+            lookup_vote_model="dateproposalvote",
+            proposal_model=DateProposal,
+        )
+        my_votes = self.get_votes(
+            context=context, vote_model=DateProposalVote, proposals=proposals
+        )
+        context["my_votes"] = my_votes
+        context["proposals"] = proposals
 
     def get_context_1(self, context):
-        proposals = self.get_proposals(context=context, lookup_vote_model='placeproposalvote',
-                                       proposal_model=PlaceProposal)
-        my_votes = self.get_votes(context=context, vote_model=PlaceProposalVote, proposals=proposals)
-        context['my_votes'] = my_votes
-        context['proposals'] = proposals
+        proposals = self.get_proposals(
+            context=context,
+            lookup_vote_model="placeproposalvote",
+            proposal_model=PlaceProposal,
+        )
+        my_votes = self.get_votes(
+            context=context, vote_model=PlaceProposalVote, proposals=proposals
+        )
+        context["my_votes"] = my_votes
+        context["proposals"] = proposals
 
     def get_context_status(self, context):
         context_status = [self.get_context_0, self.get_context_1]
@@ -75,7 +99,7 @@ class EventDetailView(UserPassesTestMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         self.get_context_status(context)
-        context['status_display'] = self.object.get_status_display()
+        context["status_display"] = self.object.get_status_display()
         #
         return context
 
