2ce4750f0303b5829d20e23ae664937be706b263
bartwisniewski <bartekwisnia@op.pl>
2023-02-13 22:03:04 +0100

    aws..

--- source/vpcoreproject/vpcoreproject/wsgi.py
+++ source/vpcoreproject/vpcoreproject/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append("/source/vpcoreproject")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vpcoreproject.settings")
 
 application = get_wsgi_application()
