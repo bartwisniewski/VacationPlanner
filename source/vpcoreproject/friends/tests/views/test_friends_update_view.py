ed857b18b0880c76c0d27a4971dd6ce41fff6710
bartwisniewski <bartekwisnia@op.pl>
2023-04-13 12:54:24 +0200

    friends tests

--- source/vpcoreproject/friends/tests/views/test_friends_update_view.py
+++ source/vpcoreproject/friends/tests/views/test_friends_update_view.py
@@ -1,19 +1,24 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 
-from users.tests.mixins import LoginRequiredViewTest
+from users.tests.mixins import SingleObjectUserRoleRequiredViewTest
 from friends.tests.data import Generator
-from friends.models import Friends, UserToFriends
 
 
-class FriendsUpdateViewTest(LoginRequiredViewTest, TestCase):
+class FriendsUpdateViewTest(SingleObjectUserRoleRequiredViewTest, TestCase):
+    redirect_url = reverse("friends-list")
+
     def setUp(self):
         super().setUp()
         self.user = self.users[0]
+
         friends_data = [("friends1",)]
         friends_generator = Generator()
         self.friends = friends_generator.generate_friends(friends_data)
         friends_generator.add_user(self.friends[0], self.user, True, True)
+        friends_generator.add_user(self.friends[1], self.users[1], False, False)
+        self.allowed_user = self.user
+        self.disallowed_user = self.users[1]
         self.url = reverse("friends-edit", kwargs={"pk": self.friends[0].id})
 
     def test_should_return_form_on_get(self):
--- source/vpcoreproject/users/tests/mixins.py
+++ source/vpcoreproject/users/tests/mixins.py
@@ -29,3 +29,31 @@ class LoginRequiredViewTest(object):
             target_status_code=200,
             fetch_redirect_response=True,
         )
+
+
+class SingleObjectUserRoleRequiredViewTest(LoginRequiredViewTest):
+    redirect_url = ""
+
+    def setUp(self):
+        self.client = Client()
+        self.users = UserGenerator().generate_users()
+        self.allowed_user = None
+        self.disallowed_user = None
+
+    def test_should_return_ok_for_allowed_user(self):
+        user = self.allowed_user
+        self.client.login(username=user.username, password=user.password)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_should_redirect_for_disallowed_user(self):
+        user = self.disallowed_user
+        self.client.login(username=user.username, password=user.password)
+        response = self.client.get(self.url)
+        self.assertRedirects(
+            response,
+            self.redirect_url,
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True,
+        )
